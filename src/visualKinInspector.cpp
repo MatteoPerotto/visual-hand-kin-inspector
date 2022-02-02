1a6e295b17dcae5704836f884afb9d7633c2ae84
Matteo Perotto <perotto.matteo@gmail.com>
2022-02-02 13:58:30 +0100

    Changed to work with hold hand encoders

--- src/visualKinInspector.cpp
+++ src/visualKinInspector.cpp
@@ -4,17 +4,79 @@
 #include<yarp/os/BufferedPort.h>
 #include<yarp/os/LogStream.h>
 #include<yarp/os/Network.h>
+#include<yarp/os/Property.h>
+#include<yarp/eigen/Eigen.h>
+#include<yarp/sig/Vector.h>
 #include<opencv4/opencv2/core.hpp>
-#include <yarp/cv/Cv.h>
+#include<yarp/cv/Cv.h>
+#include <iCub/iKin/iKinFwd.h>
+#include <unordered_map>
 
 
-EncoderReader::EncoderReader()
-{
+EncoderReader::EncoderReader(std::string robotName, std::string partName)
+{   
+    // Instantiate fingers 
+    fingers_["thumb"] = iCub::iKin::iCubFinger("left_thumb");
+    fingers_["index"] = iCub::iKin::iCubFinger("left_index");
+    fingers_["middle"] = iCub::iKin::iCubFinger("left_middle");
+    fingers_["ring"] = iCub::iKin::iCubFinger("left_ring");
+    fingers_["little"] = iCub::iKin::iCubFinger("left_little");
+
+    // Read encoders 
+    yarp::os::Property robotDriverOpt;
+    robotDriverOpt.put("device", "remote_controlboard");
+    robotDriverOpt.put("local", "/test/client");
+    robotDriverOpt.put("remote", "/" + robotName + "/" + partName); 
+    robotDriver_.open(robotDriverOpt);
+
+    robotDriver_.view(enc_);
+    enc_->getAxes(&jnts_);
+    std::cout << "Found ENCODERS: " << jnts_ << std::endl;
+
 }
 
 EncoderReader::~EncoderReader()
 {
+    robotDriver_.close();
 }
+
+Eigen::VectorXd EncoderReader::readEncoders()
+{   
+    yarp::sig::Vector encoders(jnts_); 
+    //yarp::sig::Vector fingersEncoders(9);
+    //Eigen::VectorXd armEncoders(7);
+    yarp::sig::VectorOf<double> allEncoders;
+    
+    //yarp::sig::Vector jointValues;
+    
+    // Obtain encoder values for the whole arm 
+    enc_->getEncoders(encoders.data());
+
+    // Select only hand encoders 
+    //yarp::eigen::toEigen(fingersEncoders) = yarp::eigen::toEigen(encoders);//.segment<9>(7);
+    //armEncoders = yarp::eigen::toEigen(encoders).segment<6>(0);
+
+    for (auto& finger : fingers_)
+    {
+        yarp::sig::Vector chainJoints;
+        if(finger.second.getChainJoints(encoders, chainJoints)){
+            for(int z=0; z<chainJoints.size(); z++)
+            {
+            allEncoders.push_back(chainJoints[z]);
+            }
+        }
+
+    }
+
+    Eigen::VectorXd allFingersEigen = yarp::eigen::toEigen(allEncoders) * M_PI / 180.0;
+    //Eigen::VectorXd encoderDof(armEncoders.size() + allFingersEigen.size());
+    //encoderDof << armEncoders, allFingersEigen;
+
+    //return encoderDof;
+    std::cout << "TOTAL ENCODERS " << allFingersEigen.size() << std::endl;
+    return allFingersEigen;
+}   
+
 
 // The constructor creates two ports, one reading images from the camera and the other sending images to the yarpviewer port 
 ImageStreamer::ImageStreamer(const std::string& rPortPrefix){
--- src/visualKinInspector.h
+++ src/visualKinInspector.h
@@ -1,16 +1,30 @@
+#include<Eigen/Core>
 #include<iostream>
+#include<opencv4/opencv2/core.hpp>
+#include<yarp/dev/IEncoders.h>
+#include<yarp/dev/PolyDriver.h>
 #include<yarp/os/Network.h>
 #include<yarp/os/BufferedPort.h>
 #include<yarp/sig/Image.h>
-#include<opencv4/opencv2/core.hpp>
+#include <iCub/iKin/iKinFwd.h>
+#include <unordered_map>
 
 class EncoderReader
 {
     public:
-    EncoderReader();
+    EncoderReader(std::string robotName, std::string partName);
     ~EncoderReader();
-    
+    Eigen::VectorXd readEncoders();
+    std::unordered_map<std::string, iCub::iKin::iCubFinger> fingers_;
+
+    private:
+    yarp::dev::PolyDriver robotDriver_;
+    yarp::dev::PolyDriver analogDriver_;
+    int jnts_;
+    yarp::dev::IEncoders *enc_;
+    //Eigen::Map<Eigen::VectorXd> encoderReads_;
 };
+
 
 class ImageStreamer
 {
@@ -23,6 +37,5 @@ class ImageStreamer
     yarp::os::Network yarp_;
     yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> rgbPortIn_;
     yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> rgbPortOut_;
-
 };
 
